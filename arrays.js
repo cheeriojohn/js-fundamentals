4eed403151efdb77b0db915e05ccd678f51c7b26
Nick White <NickW@macbook-pro.home>
2022-11-18 16:44:25 +0000

    first commit

---
+++ arrays.js
@@ -0,0 +1,76 @@
+// console.log("hello");
+// let faveHolidayDestinations = [
+//     "Bognor regis, England".toLocaleUpperCase(),
+//     "Skegness, England",
+//     "Southend-on-sea, England"
+// ];
+// console.log(faveHolidayDestinations);
+
+
+// let faveFilms = [
+//     "ET",
+//     "\n",
+//     "Independence Day",
+//     "Dunkirk"
+// ];
+// console.log(faveFilms);
+// console.log(faveFilms[3]);
+// faveFilms.push("carry on doctor");
+// console.log(faveFilms);
+// faveFilms.shift();
+// console.log(faveFilms)
+// faveFilms.push("ET");
+// console.log(faveFilms)
+// faveFilms.unshift("The Jungle Book");
+// console.log(faveFilms)
+// console.log(faveFilms.at(-1));
+// console.log(faveFilms.pop());
+// console.log(faveFilms);
+// faveFilms[1] = "Far from teh Madding Crowd";
+// console.log(faveFilms);
+// console.log(faveFilms.length)
+// console.log(faveFilms.pop());
+// console.log(faveFilms)
+// console.log(faveFilms.toString().replace(/,/g, ''));
+
+
+// let faveSongs = [
+//     "Waterloo Sunset",
+//     "Hush",
+//     "Grandmaster Flash",
+//     "new song 1",
+//     "new song 2",
+// ]
+// // for (let i=3; i<faveSongs.length; i++);
+// // console.log(faveSongs[i]);
+
+// for (let i = 0; i <= 100; i++) {
+//     console.log((i += 2) - 1);
+// }
+
+// let evenNumbers = [];
+
+// for (let n = 0; n < 20; n++) {
+//     if (n % 2 == 0) {
+//         evenNumbers.push(n)
+//     };
+//   };
+// console.log(evenNumbers)
+
+// let lives = 3;
+
+// while (lives > 0) {
+
+// }
+
+
+let oddNumbers = [];
+
+for (let i = 0; i < 20; i++) {
+    if (i % 2 != 0) {
+
+       oddNumbers.push(i)
+    }
+}
+console.log(oddNumbers);
+//console.log(i);  // THIS SAYS i IS NOT DEFINED
---
+++ js_fundamentals.js
@@ -0,0 +1,21 @@
+// ACTIVITY No.1
+
+console.log("\nACTIVITY No.1\n")
+
+const sayHello = () => {
+    console.log("Hello, you. Are you in need of some console-ation?");
+    return "Well, hi there, old bean. Happy returns!";
+};
+console.log(sayHello());
+
+// ACTIVITY No.2
+
+console.log("\nACTIVITY No.2\n")
+
+const sayHelloAgain = (yrName, drink) => {
+    console.log("Hello, you");
+    return console.log(`Well I never...if it isn't old ${yrName}. How the devil are you? Let me pay for your ${drink}!`);
+};
+sayHelloAgain("Gasper","champagne");
+
+console.log("That's all from this particular venture into functions...")
